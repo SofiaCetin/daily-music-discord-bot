c5df5c113d18f34a0da8927fca0da8a08bd67088
Sofia <sofia@Host-002.lan>
2026-03-31 02:14:34 +0200

    correction sgbd

--- db.py
+++ db.py
@@ -26,36 +26,49 @@ def db_init():
     cur.close()
     conn.close()
 
-conn = connect()
-
-cur = conn.cursor()
-
 def save_state(discord_id,state):
+    conn = connect()
+    cur = conn.cursor()
+
     cur.execute("""
         INSERT INTO linked_users (discord_id, state)
         VALUES (%s, %s)
         ON CONFLICT(discord_id) DO UPDATE SET state = excluded.state
     """, (discord_id, state))
 
+    conn.commit()
+    cur.close()
+    conn.close()
+
 def check_user_state(discord_id,state):
+    conn = connect()
+    cur = conn.cursor()
+
     cur.execute("""
         SELECT state 
         FROM linked_users 
         WHERE discord_id = %s
     """, (discord_id,))
     res = cur.fetchone()
+    cur.close()
+    conn.close()
     if res[0] == state:
         return True
     else:
         return False
 
 def check_state_exists(state):
+    conn = connect()
+    cur = conn.cursor()
+
     cur.execute("""
         SELECT discord_id
         FROM linked_users
         WHERE state = %s
     """, (state,))
     ligne = cur.fetchone()
+    cur.close()
+    conn.close()
     if ligne:
         discord_id = ligne[0]
         print(f"Le state existe pour l'ID discord {discord_id}")
@@ -65,53 +78,81 @@ def check_state_exists(state):
         return None
 
 def get_state_expiration(state):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         SELECT state_expiration
         FROM linked_users
         WHERE state = %s
 """, (state, ))
+    cur.close()
+    conn.close()
     
 def get_refresh_token(discord_id):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         SELECT refresh_token 
         FROM linked_users 
         WHERE discord_id = %s
     """,(discord_id,))
     res = cur.fetchone()
+    cur.close()
+    conn.close()
     if res:
         return res[0]
     else:
         return None
 
 def get_access_token(discord_id):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         SELECT access_token
         FROM linked_users
         WHERE discord_id = %s
     """, (discord_id,))
     res = cur.fetchone()
+    cur.close()
+    conn.close()
     if res:
         return res[0]
     else:
         return None
     
 def add_new_refresh_token(state, refresh_token):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         UPDATE linked_users
         SET refresh_token = %s
         WHERE state = %s
 """,(refresh_token, state))
+    conn.commit()
+    cur.close()
+    conn.close()
+    
     
 def add_new_token(state, access_token, expires_at):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         UPDATE linked_users
         SET access_token = %s, expires_at = %s
         WHERE state = %s          
     """, (access_token ,expires_at ,state))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 def delete_state(state):
+    conn = connect()
+    cur = conn.cursor()
     cur.execute("""
         UPDATE linked_users
         SET state = NULL
         WHERE state = %s
 """, (state, ))
+    conn.commit()
+    cur.close()
+    conn.close()
