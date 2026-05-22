5a0a0e3c05f574c9c2bdb4b406780e702e131772
Sofia <sofia@Host-002.lan>
2026-05-22 20:57:33 +0200

    retrait de la fonction create app

--- src/app.py
+++ src/app.py
@@ -18,49 +18,45 @@ REDIRECT_URI = "https://flask-production-92a6.up.railway.app/callback"
 AUTH_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token"
 
-# App
+app = Flask(__name__)
+app.secret_key = APP_SECRET
 
-def create_app():
+db.db_init()
+
+# Routes
     
-    app = Flask(__name__)
-    app.secret_key = APP_SECRET
+@app.route('/')
+def index():
+    return "Bot is online"
 
-    db.db_init()
-
-    @app.route('/')
-    def index():
-        return "Bot is online"
-
-    @app.route("/callback")
-    def callback():
-        if "error" in request.args:
-            return jsonify({"error": request.args["error"]})
+@app.route("/callback")
+def callback():
+    if "error" in request.args:
+        return jsonify({"error": request.args["error"]})
         
-        if not "state" in request.args:
-            return "State error: No state found"
+    if not "state" in request.args:
+        return "State error: No state found"
         
-        state = request.args["state"]
-        discord_id = db.check_state_exists(state)
-        if not discord_id:
-            return "State error: state expired or invalid"
+    state = request.args["state"]
+    discord_id = db.check_state_exists(state)
+    if not discord_id:
+        return "State error: state expired or invalid"
         
-        if "code" in request.args:
-            req_body = {
-                "code" : request.args["code"],
-                "grant_type" : "authorization_code",
-                "redirect_uri" : REDIRECT_URI,
-                "client_id" : CLIENT_ID,
-                "client_secret" : CLIENT_SECRET
-            }
+    if "code" in request.args:
+        req_body = {
+            "code" : request.args["code"],
+            "grant_type" : "authorization_code",
+            "redirect_uri" : REDIRECT_URI,
+            "client_id" : CLIENT_ID,
+            "client_secret" : CLIENT_SECRET
+        }
 
-            response = requests.post(TOKEN_URL,data=req_body)
-            token_info = response.json()
-            db.add_new_refresh_token(discord_id, token_info["refresh_token"])
-            db.add_new_token(discord_id, token_info["access_token"], datetime.datetime.now().timestamp() + token_info["expires_in"])
-            db.delete_state(discord_id)
+        response = requests.post(TOKEN_URL,data=req_body)
+        token_info = response.json()
+        db.add_new_refresh_token(discord_id, token_info["refresh_token"])
+        db.add_new_token(discord_id, token_info["access_token"], datetime.datetime.now().timestamp() + token_info["expires_in"])
+        db.delete_state(discord_id)
 
-            return "State valid"
-        else:
-            return "No body"
-    
-    return app
+        return "State valid"
+    else:
+        return "No body"
